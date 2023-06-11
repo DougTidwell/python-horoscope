82fb8a14ca9d4161665e1ee3ed2e98ed37b48144
Doug Tidwell <doug_tidwell@yahoo.com>
2023-06-11 13:33:10 -0400

    Cleaned up the imports, etc.

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 A python implementation of the horoscope app. The four `.py` files deliver 
 four different horoscopes: 
 
-* **`kafkaesque`** - Sad. Always ends with the phrase "Your life will be bleak and miserable." 
-* **`ominous`** - Scary. Example: "The stars say to treat yourself to a lavish dinner tonight. Their exact words: 'As if it were your last meal.'"
 * **`optimistic`** - Cheery and upbeat. Example: "The stars say to treat yourself to a lavish dinner tonight." 
+* **`ominous`** - Scary. Example: "The stars say to treat yourself to a lavish dinner tonight. Their exact words: 'As if it were your last meal.'"
+* **`kafkaesque`** - Sad. Always ends with the phrase "Your life will be bleak and miserable." 
 * **`planets_in_motion`** - Determined by the movement of planets and stars. Example: "Mercury said to tell you not to call the cops, or else. The stars assume you know what that's all about." 
 
 Each version opens port 5000. The REST endpoint is `host:5000/horoscope/{sign}` with `sign` being one of the twelve (case-insensitive) signs of the zodiac. 
--- optimistic.py
+++ optimistic.py
@@ -1,83 +1,83 @@
-from flask import Flask, jsonify
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/horoscope/<sign>')
 def getHoroscope(sign):
     match sign.lower():
         case "aries":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars suggest you talk to your friends and loved ones today.",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "taurus":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say it\'s time for a vacation!",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "gemini":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say you should put your irrational fears behind you and make the most of the day!",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "cancer":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say something hilarious will happen to you today. Laugh hard!",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "leo":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say your enemies will forgive you today.",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "virgo":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say something will weigh heavily on your mind. Stay positive!",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "libra":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say to stand up for yourself - today is no time to back down. You can do this!",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "scorpio":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say you should be grateful for all the wonderful friends in your life. ",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "sagittarius":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say a financial windfall could be yours today.",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "capricorn":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say you should treat yourself to a lavish dinner tonight. You\'ve earned it!",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "aquarius":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say you should make a generous, unexpected gift to someone you barely know today.",
                 provider="Planetary Motion",
                 css="color: green;"
             )
         case "pisces":
-            return jsonify(
+            return flask.jsonify(
                 horoscope="The stars say to approach a confrontation with kid gloves and everything will be fine.",
                 provider="Planetary Motion",
                 css="color: green;"
             )
-    return jsonify(
+    return flask.jsonify(
         horoscope="Sorry, you need to tell me your sign.",
         provider="Planetary Motion",
         css="color: red;"

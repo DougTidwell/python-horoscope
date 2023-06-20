263fcf14fc9d660fa4984411bdfa31f20f8f3847
Doug Tidwell <doug_tidwell@yahoo.com>
2023-06-20 08:52:23 -0400

    Minor changes, source reformatting, etc.

--- kafkaesque.py
+++ kafkaesque.py
@@ -4,85 +4,85 @@ app = flask.Flask(__name__)
 
 @app.route('/horoscope/<sign>')
 def getHoroscope(sign):
-    match sign.lower():
-        case "aries":
-            return flask.jsonify(
-                horoscope="You will be arrested today. You will never be told the nature of the charges against you. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "taurus":
-            return flask.jsonify(
-                horoscope="You will feel your father\'s disapproval keenly today. Pretty much like every other day. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "gemini":
-            return flask.jsonify(
-                horoscope="After tomorrow, let\'s just say you\'ll be wearing shoes six at a time. Shop accordingly. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "cancer":
-            return flask.jsonify(
-                horoscope="You will write the person you love, telling them your relationship will never happen. They will never write back. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "leo":
-            return flask.jsonify(
-                horoscope="The excitement of finding a new form of disappointment will fade quickly. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "virgo":
-            return flask.jsonify(
-                horoscope="You will have a sudden insight that will bring you lasting peace and contentment. Just kidding. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "libra":
-            return flask.jsonify(
-                horoscope="Your lawyer will assure you that this will all be over soon. He is correct, but not in a good way. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "scorpio":
-            return flask.jsonify(
-                horoscope="What happens today won\'t be any more bearable if the stars tell you about it beforehand, so let\'s leave it at that. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "sagittarius":
-            return flask.jsonify(
-                horoscope="Not everybody would keep pursuing an unobtainable goal, but you seem to have a real flair for it. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "capricorn":
-            return flask.jsonify(
-                horoscope="You will start the day with a really good cappuccino. Like, seriously good. But it\'ll all be downhill from there. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "aquarius":
-            return flask.jsonify(
-                horoscope="You don\'t have a dog, but even if you did, it wouldn\'t like you. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "pisces":
-            return flask.jsonify(
-                horoscope="Your city will have an amazing zoo, but you\'ll never live to see it. Your life will be bleak and miserable.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-    return flask.jsonify(
-        horoscope="Sorry, you need to tell me your sign.",
+  match sign.lower():
+    case "aries":
+      return flask.jsonify(
+        horoscope="You will be arrested today. You will never be told the nature of the charges against you. Your life will be bleak and miserable.",
         provider="Planetary Motion",
-        css="color: red;"
-    )
+        css="color: green;"
+      )
+    case "taurus":
+      return flask.jsonify(
+        horoscope="You will feel your father\'s disapproval keenly today. Pretty much like every other day. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "gemini":
+      return flask.jsonify(
+        horoscope="After tomorrow, let\'s just say you\'ll be wearing shoes six at a time. Shop accordingly. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "cancer":
+      return flask.jsonify(
+        horoscope="You will write the person you love, telling them your relationship will never happen. They will never write back. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "leo":
+      return flask.jsonify(
+        horoscope="The excitement of finding a new form of disappointment will fade quickly. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "virgo":
+      return flask.jsonify(
+        horoscope="You will have a sudden insight that will bring you lasting peace and contentment. Just kidding. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "libra":
+      return flask.jsonify(
+        horoscope="Your lawyer will assure you that this will all be over soon. He is correct, but not in a good way. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "scorpio":
+      return flask.jsonify(
+        horoscope="What happens today won\'t be any more bearable if the stars tell you about it beforehand, so let\'s leave it at that. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "sagittarius":
+      return flask.jsonify(
+        horoscope="Not everybody would keep pursuing an unobtainable goal, but you seem to have a real flair for it. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "capricorn":
+      return flask.jsonify(
+        horoscope="You will start the day with a really good cappuccino. Like, seriously good. But it\'ll all be downhill from there. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "aquarius":
+      return flask.jsonify(
+        horoscope="You don\'t have a dog, but even if you did, it wouldn\'t like you. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "pisces":
+      return flask.jsonify(
+        horoscope="Your city will have an amazing zoo, but you\'ll never live to see it. Your life will be bleak and miserable.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+  return flask.jsonify(
+    horoscope="Sorry, you need to tell me your sign.",
+    provider="Planetary Motion",
+    css="color: red;"
+  )
 
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
--- optimistic.py
+++ optimistic.py
@@ -4,85 +4,85 @@ app = flask.Flask(__name__)
 
 @app.route('/horoscope/<sign>')
 def getHoroscope(sign):
-    match sign.lower():
-        case "aries":
-            return flask.jsonify(
-                horoscope="The stars suggest you talk to your friends and loved ones today.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "taurus":
-            return flask.jsonify(
-                horoscope="The stars say it\'s time for a vacation!",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "gemini":
-            return flask.jsonify(
-                horoscope="The stars say you should put your irrational fears behind you and make the most of the day!",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "cancer":
-            return flask.jsonify(
-                horoscope="The stars say something hilarious will happen to you today. Laugh hard!",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "leo":
-            return flask.jsonify(
-                horoscope="The stars say your enemies will forgive you today.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "virgo":
-            return flask.jsonify(
-                horoscope="The stars say something will weigh heavily on your mind. Stay positive!",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "libra":
-            return flask.jsonify(
-                horoscope="The stars say to stand up for yourself - today is no time to back down. You can do this!",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "scorpio":
-            return flask.jsonify(
-                horoscope="The stars say you should be grateful for all the wonderful friends in your life. ",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "sagittarius":
-            return flask.jsonify(
-                horoscope="The stars say a financial windfall could be yours today.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "capricorn":
-            return flask.jsonify(
-                horoscope="The stars say you should treat yourself to a lavish dinner tonight. You\'ve earned it!",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "aquarius":
-            return flask.jsonify(
-                horoscope="The stars say you should make a generous, unexpected gift to someone you barely know today.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-        case "pisces":
-            return flask.jsonify(
-                horoscope="The stars say to approach a confrontation with kid gloves and everything will be fine.",
-                provider="Planetary Motion",
-                css="color: green;"
-            )
-    return flask.jsonify(
-        horoscope="Sorry, you need to tell me your sign.",
+  match sign.lower():
+    case "aries":
+      return flask.jsonify(
+        horoscope="The stars suggest you talk to your friends and loved ones today.",
         provider="Planetary Motion",
-        css="color: red;"
-    )
+        css="color: green;"
+      )
+    case "taurus":
+      return flask.jsonify(
+        horoscope="The stars say it\'s time for a vacation!",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "gemini":
+      return flask.jsonify(
+        horoscope="The stars say you should put your irrational fears behind you and make the most of the day!",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "cancer":
+      return flask.jsonify(
+        horoscope="The stars say something hilarious will happen to you today. Laugh hard!",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "leo":
+      return flask.jsonify(
+        horoscope="The stars say your enemies will forgive you today.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "virgo":
+      return flask.jsonify(
+        horoscope="The stars say something will weigh heavily on your mind. Stay positive!",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "libra":
+      return flask.jsonify(
+        horoscope="The stars say to stand up for yourself - today is no time to back down. You can do this!",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "scorpio":
+      return flask.jsonify(
+        horoscope="The stars say you should be grateful for all the wonderful friends in your life. ",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "sagittarius":
+      return flask.jsonify(
+        horoscope="The stars say a financial windfall could be yours today.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "capricorn":
+      return flask.jsonify(
+        horoscope="The stars say you should treat yourself to a lavish dinner tonight. You\'ve earned it!",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "aquarius":
+      return flask.jsonify(
+        horoscope="The stars say you should make a generous, unexpected gift to someone you barely know today.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+    case "pisces":
+      return flask.jsonify(
+        horoscope="The stars say to approach a confrontation with kid gloves and everything will be fine.",
+        provider="Planetary Motion",
+        css="color: green;"
+      )
+  return flask.jsonify(
+    horoscope="Sorry, you need to tell me your sign.",
+    provider="Planetary Motion",
+    css="color: red;"
+  )
 
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
